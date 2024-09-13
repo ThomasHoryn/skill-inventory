680a204761184636dc984b3fe0db0df94591a42b
Tomasz Horyn <tomasz.t.horyn@gmail.com>
2024-09-13 08:30:50 +0200

    chore: change login styles to primeng

--- src/main/webapp/app/login/login.component.ts
+++ src/main/webapp/app/login/login.component.ts
@@ -5,12 +5,41 @@ import { Router, RouterModule } from '@angular/router';
 import SharedModule from 'app/shared/shared.module';
 import { LoginService } from 'app/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
+import { MessagesModule } from 'primeng/messages';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslationService } from '../shared/language/translation.service';
+import { Message } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { Button, ButtonDirective } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   standalone: true,
   selector: 'jhi-login',
-  imports: [SharedModule, FormsModule, ReactiveFormsModule, RouterModule],
-  templateUrl: './login.component.html',
+  imports: [
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MessagesModule,
+    InputTextModule,
+    ButtonDirective,
+    Ripple,
+    CheckboxModule,
+    DividerModule,
+    Button,
+  ],
+  templateUrl: './p-login.component.html',
+  animations: [
+    trigger('fade', [
+      state('visible', style({ display: 'block', opacity: 1 })),
+      state('hidden', style({ display: 'none', opacity: 0 })),
+      transition('visible <=> hidden', animate('500ms ease-in-out')),
+    ]),
+  ],
 })
 export default class LoginComponent implements OnInit, AfterViewInit {
   username = viewChild.required<ElementRef>('username');
@@ -26,6 +55,10 @@ export default class LoginComponent implements OnInit, AfterViewInit {
   private accountService = inject(AccountService);
   private loginService = inject(LoginService);
   private router = inject(Router);
+  private translateService = inject(TranslateService);
+  private translationService = inject(TranslationService);
+
+  authenticationErrorMsg: Message[] = [];
 
   ngOnInit(): void {
     // if already authenticated then navigate to home page
@@ -34,10 +67,28 @@ export default class LoginComponent implements OnInit, AfterViewInit {
         this.router.navigate(['']);
       }
     });
+    this.translateService.onLangChange.subscribe(() => {
+      this.updateErrorItems();
+    });
+
+    this.updateErrorItems();
   }
 
   ngAfterViewInit(): void {
     this.username().nativeElement.focus();
+  }
+
+  private updateErrorItems() {
+    this.authenticationErrorMsg = [
+      {
+        severity: 'error',
+        detail: this.getTranslation('login.messages.error.authentication'),
+      },
+    ];
+  }
+
+  getTranslation(description: string): string {
+    return this.translationService.getTranslation(description);
   }
 
   login(): void {
